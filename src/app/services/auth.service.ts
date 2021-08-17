dc4603203b5ee25a34a86b71023af2fdca289fe0
GianmarcoSetzu1 <setzu.gianmarco@yahoo.it>
2021-08-17 18:10:08 +0200

    first commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
+
+import { User } from "../models/User";
+import {BehaviorSubject, Observable} from "rxjs";
+import {catchError, first} from "rxjs/operators";
+import {ErrorHandlerService} from "./error-handler.service";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private url = "http://localhost:8000/auth";
+
+  isUserLoggedIn$ = new BehaviorSubject<boolean>(false);
+  userUsername: Pick<User, "id">;
+
+
+  httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" }),
+  };
+
+  constructor(
+    private http: HttpClient,
+    private errorHandlerService: ErrorHandlerService,
+    private router: Router
+  ) {}
+
+
+  signup(user: Omit<User, "username">): Observable<User> {
+    return this.http
+      .post<User>(`${this.url}/signup`, user, this.httpOptions)
+      .pipe(
+        first(),
+        catchError(this.errorHandlerService.handleError<User>("signup"))
+      );
+  }
+}
